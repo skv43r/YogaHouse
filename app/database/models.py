1b33d6609291ee52b8c67222abe465eebe4691e4
Alexandr <skv43r@gmail.com>
2024-11-24 16:18:20 +0300

    feat(models): create GroupSpecialists and GroupServices models

--- app/database/models.py
+++ app/database/models.py
@@ -18,3 +18,16 @@ class Massage(SQLModel, table=True):
     name: str = Field(index=True)
     description: str | None = Field(default=None)
     price: int | None = Field(default=None, index=True)
+
+class GroupSpecialists(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    description: str | None = Field(default=None)
+    photo: str | None = Field(default=None)
+
+class GroupServices(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    description: str | None = Field(default=None)
+    price: int | None = Field(default=None, index=True)
+    photo: str | None = Field(default=None)
