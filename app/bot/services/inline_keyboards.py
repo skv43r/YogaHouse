0529d334b1d0ef1ab3575c1f8066b0f1b54f9053
Alexandr <skv43r@gmail.com>
2024-11-25 15:15:18 +0300

    feat(inline-keyboards): create admin_chat keyboard and about keyboard

--- app/bot/services/inline_keyboards.py
+++ app/bot/services/inline_keyboards.py
@@ -1,4 +1,5 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 from database.models import IndividualSpecialists, IndividualServices, Massage
 from database.database import db
 from sqlmodel import select
@@ -31,6 +32,24 @@ def generate_time_keyboard():
 
     return InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
 
+def generate_admin_chat_keyboard():
+    url = "https://t.me/s3jke"
+    return InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Открыть чат", url=url,  callback_data=f"admin_chat")]
+])
+
+def generate_about_keyboard():
+    map_url = "https://yandex.ru/maps/-/CDxPyQ8T"
+    site = "http://www.yogahom.ru/"
+    whatsup = "https://wa.me/79298274034?text=%D0%9F%D1%80%D0%B8%D0%B2%D0%B5%D1%82%2C%20%D0%BC%D0%B5%D0%BD%D0%B5%D0%B4%D0%B6%D0%B5%D1%80%20%D0%BF%D0%BE%D0%BC%D0%BE%D0%B6%D0%B5%D1%82%20%D1%82%D0%B5%D0%B1%D0%B5%20%D0%B2%D1%8B%D0%B1%D1%80%D0%B0%D1%82%D1%8C%20%D0%BF%D0%BE%D0%B4%D1%85%D0%BE%D0%B4%D1%8F%D1%89%D0%B5%D0%B5%20%D0%BD%D0%B0%D0%BF%D1%80%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5%20%D0%B9%D0%BE%D0%B3%D0%B8%20%D0%B8%20%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D0%B0%D1%82%D1%8C%20%D0%BD%D0%B0%20%D0%B7%D0%B0%D0%BD%D1%8F%D1%82%D0%B8%D0%B5%20%D0%B2%20%D1%83%D0%B4%D0%BE%D0%B1%D0%BD%D1%83%D1%8E%20%D0%B4%D0%B0%D1%82%D1%83%21"
+    return InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Whats Up", url=whatsup)],
+    [InlineKeyboardButton(text="www.yogahom.ru", url=site)],
+    [InlineKeyboardButton(text="Улица Северная, 528а", url=map_url, callback_data=f"send_location")]
+])
+
+
+
 def generate_keyboards():
     with next(db.get_session()) as session:
         individual_specialists = get_items_from_db(session, IndividualSpecialists)
@@ -48,3 +67,5 @@ def generate_keyboards():
 
 individual_specialists_keyboard, individual_services_keyboard, massage_list_keyboard, massage_master_keyboard = generate_keyboards()
 time_keyboard = generate_time_keyboard()
+admin_chat_keyboard = generate_admin_chat_keyboard()
+about_keyboard = generate_about_keyboard()
