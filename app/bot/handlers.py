e8945b85ac21434061ffadee866009592520277e
Alexandr <skv43r@gmail.com>
2024-11-25 15:13:53 +0300

    feat(handlers): add handlers to inline butons

--- app/bot/handlers.py
+++ app/bot/handlers.py
@@ -4,8 +4,12 @@ from aiogram.filters.callback_data import CallbackData
 from aiogram.types import CallbackQuery
 from aiogram_calendar import SimpleCalendar, SimpleCalendarCallback
 from aiogram.fsm.context import FSMContext
-from services.menu import menu_keyboard, individual_service_keyboard, type_keyboard
-from services.inline_keyboards import individual_specialists_keyboard, individual_services_keyboard, massage_list_keyboard, massage_master_keyboard, time_keyboard
+from sqlalchemy import select
+from services.menu import menu_keyboard, type_keyboard
+from services.inline_keyboards import individual_specialists_keyboard, individual_services_keyboard, massage_list_keyboard, massage_master_keyboard, time_keyboard, admin_chat_keyboard, about_keyboard
+
+from database.models import IndividualSpecialists, IndividualServices, Massage, Booking
+from database.database import db
 
 class CallbackDataForSpecialist(CallbackData, prefix="ind_spec"):
     specialist_id: int
@@ -18,39 +22,37 @@ def register_handlers(dp: Dispatcher):
 
     @dp.message(lambda message: message.text == "Индивидуальные услуги")
     async def handle_individual_services_menu(message: types.Message):
-        await message.answer("Выберите опцию:", reply_markup=individual_service_keyboard)
+        await message.answer("Выберите опцию:", reply_markup=type_keyboard)
     
     @dp.message(lambda message: message.text == "Главное меню")
     async def handle_main_menu(message: types.Message):
         await message.answer("Выберите опцию:", reply_markup=menu_keyboard)
     
-    @dp.message(lambda message: message.text == "Выбрать услуги")
-    async def handle_services_type_menu(message: types.Message):
-        await message.answer("Выберите тип услуги:", reply_markup=type_keyboard)
-
     @dp.message(lambda message: message.text == "Индивидуальные занятия")
     async def handle_individual_services_menu(message: types.Message):
             await message.answer("Выберите услугу:", reply_markup=individual_services_keyboard)
 
-    
-# Выбрать специалиста
-
-    @dp.message(lambda message: message.text == "Выбрать специалиста")
-    async def handle_individual_specialists_menu(message: types.Message):
-        await message.answer("Выберите специалиста:", reply_markup=individual_specialists_keyboard)
-
-
 # Выбор услуги
 
-    @dp.callback_query(lambda c: c.data and c.data.startswith("individual_specialist"))
-    async def handle_specific_service(callback_query: types.CallbackQuery):
+    @dp.callback_query(lambda c: c.data and c.data.startswith("individual_services_"))
+    async def handle_specific_service(callback_query: types.CallbackQuery, state: FSMContext):
+        service_id = callback_query.data.split("_")[-1]
+        with next(db.get_session()) as session:
+            query = select(IndividualServices.name).where(IndividualServices.id == service_id)
+            name = session.exec(query).scalar()
+            await state.update_data(selected_service=name)
         await callback_query.message.edit_text(
-            text=f"Выберите услугу:",
-            reply_markup=individual_services_keyboard
+            text=f"Выберите специалиста:",
+            reply_markup=individual_specialists_keyboard
         )
 
-    @dp.callback_query(lambda c: c.data and c.data.startswith("individual_services_"))
-    async def handle_specific_specialist(callback_query: types.CallbackQuery):
+    @dp.callback_query(lambda c: c.data and c.data.startswith("individual_specialist"))
+    async def handle_specific_specialist(callback_query: types.CallbackQuery, state: FSMContext):
+        master_id = callback_query.data.split("_")[-1]
+        with next(db.get_session()) as session:
+            query = select(IndividualSpecialists.name).where(IndividualSpecialists.id == master_id)
+            name = session.exec(query).scalar()
+            await state.update_data(selected_master=name)
         await callback_query.message.edit_text(
             text=f"Выберите дату:",
             reply_markup= await SimpleCalendar().start_calendar()
@@ -63,14 +65,26 @@ def register_handlers(dp: Dispatcher):
         await message.answer("Выберите специалиста:", reply_markup=massage_list_keyboard)
     
     @dp.callback_query(lambda c: c.data and c.data.startswith("massage_list"))
-    async def handle_massage_list(callback_query: types.CallbackQuery):
+    async def handle_massage_list(callback_query: types.CallbackQuery, state: FSMContext):
+        massage_id = callback_query.data.split("_")[-1]
+        with next(db.get_session()) as session:
+            query = select(Massage.name).where(Massage.id == massage_id)
+            name = session.exec(query).scalar()
+            await state.update_data(selected_service=name)
+        
         await callback_query.message.edit_text(
             text=f"Выберите мастера:",
             reply_markup=massage_master_keyboard
         )
 
     @dp.callback_query(lambda c: c.data and c.data.startswith("massage_master"))
-    async def handle_date_check(callback_query: types.CallbackQuery):
+    async def handle_date_check(callback_query: types.CallbackQuery, state: FSMContext):
+        master_id = callback_query.data.split("_")[-1]
+        with next(db.get_session()) as session:
+            query = select(IndividualSpecialists.name).where(IndividualSpecialists.id == master_id)
+            name = session.exec(query).scalar()
+            await state.update_data(selected_master=name)
+
         await callback_query.message.edit_text(
             text=f"Выберите дату:",
             reply_markup= await SimpleCalendar().start_calendar()
@@ -90,7 +104,71 @@ def register_handlers(dp: Dispatcher):
     async def process_time_selection(callback_query: CallbackQuery, state: FSMContext):
         selected_time = callback_query.data.split("_")[1]
         user_data = await state.get_data()
-        selected_date = user_data.get("selected_date", "дата не выбрана")
+        selected_date = user_data.get("selected_date")
+        selected_master = user_data.get("selected_master")
+        selected_service = user_data.get("selected_service")
+        user_id = callback_query.from_user.id
         await callback_query.message.edit_text(
-            text=f"Вы записаны на {selected_date} в {selected_time}.\nЖдем вас!"
+            text=(
+                f"Вы записаны на: *{selected_service}*\n"
+                f"К мастеру: *{selected_master}*\n"
+                f"Ждем вас *{selected_date}* в *{selected_time}*!"
+            ),
+            parse_mode="Markdown"
+        )
+        with next(db.get_session()) as session:
+            new_booking = Booking(
+                user_id=user_id,
+                service=selected_service,
+                master=selected_master,
+                date=selected_date,
+                time=selected_time
+            )
+            session.add(new_booking)
+            session.commit()
+
+    @dp.message(lambda message: message.text == "Чат с администратором")
+    async def handle_admin_chat(message: types.Message):
+        await message.answer("Связаться с админимстратором", reply_markup=admin_chat_keyboard)
+
+    @dp.message(lambda message: message.text == "Мои записи")
+    async def handle_my_bookings(message: types.Message):
+        user_id = message.from_user.id
+        with next(db.get_session()) as session:
+            bookings = session.scalars(select(Booking).where(Booking.user_id == user_id)).all()
+            if not bookings:
+                await message.answer("У вас пока нет записей.")
+            else:
+                response = "Ваши записи:\n\n"
+                for booking in bookings:
+                    print(f"Тип объекта: {type(booking)}")
+                    print(f"Атрибуты: {dir(booking)}")
+                    print(f"Данные: {booking}")
+                    
+                    response += (
+                        f"Услуга: *{booking.service}*\n"
+                        f"Мастер: *{booking.master}*\n"
+                        f"Дата: *{booking.date}*\n"
+                        f"Время: *{booking.time}*\n\n"
+                    )
+
+                await message.answer(response, parse_mode="Markdown")
+
+    @dp.message(lambda message: message.text == "О филиале")
+    async def handle_about(message: types.Message):
+        await message.answer(
+            text=
+"""Студия йоги в центре Краснодара - *Йога Хом*
+
+*Часы работы:*
+Пн. - Вс.: 9:00-21:00
+
+*Контакты:*
+[+7 929 827-40-24](tel:+79298274024)
+
+Попробуйте захватывающие занятия йогой в гамаках, улучшите гибкость и силу с _Аштанга йогой_, насладитесь мягкими практиками _Soft йоги_ или восстановите здоровье спины с помощью _Йогатерапии позвоночника_. Присоединяйтесь к нам и найдите свой путь к внутреннемуравновесию и благополучию.
+
+Возраст и уровень подготовки не важен, мы ждём всех, до встречи на ковриках!""",
+            parse_mode="Markdown",
+            reply_markup=about_keyboard
         )
